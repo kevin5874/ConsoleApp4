3eaae55adb182b0c70ea6f78c4e1f8f8588e0c49
kevin5874 <39664646+kevin5874@users.noreply.github.com>
2018-07-05 22:39:46 -0500

    Tech academy drill

---
+++ ConsoleApp4/HourlyPay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class HourlyPay
+    {
+        private int hoursWorked { get; set; }
+        private double hourlyRate { get; set; }
+
+        public HourlyPay(int hoursWorked, double hourlyRate)
+            {
+            this.hourlyRate = hourlyRate;
+            this.hoursWorked = hoursWorked;
+            }
+
+        public double GetTotalPay()
+        {
+            double result = hourlyRate * hoursWorked;
+            return result;
+        }
+    }
+}
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            HourlyPay Person1 = new HourlyPay(Int32.Parse(Console.ReadLine()), Double.Parse(Console.ReadLine()));
+            Console.WriteLine("Person 2");
+            HourlyPay Person2 = new HourlyPay(Int32.Parse(Console.ReadLine()), Double.Parse(Console.ReadLine()));
+            Console.WriteLine("Annual salary of Person 1: {0}" , Person1.GetTotalPay());
+            Console.WriteLine("Annual salary of Person 2: {0}", Person2.GetTotalPay());
+            bool compare = Person1.GetTotalPay() > Person2.GetTotalPay();
+            Console.WriteLine("Does Person 1 make more money than person 2: {0}", compare);
+            Console.ReadKey();
+           
         }
     }
 }
